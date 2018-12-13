550ef77bc4e90300c7b98dce390368704f81d6c2
Trevor Adcock <trevor.adcock@mac.com>
2018-12-13 16:51:32 -0700

    Initial Commit

---
+++ DripDrop2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DripDrop2.0
+//
+//  Created by DevMountain on 12/13/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
